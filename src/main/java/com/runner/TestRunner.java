a731c7784f9e7160edeb91833ebf8d8adb4d90cd
Aman <amandaharwal@gmail.com>
2024-03-28 11:14:34 +0530

    Updated TestRunner

--- src/main/java/com/runner/TestRunner.java
+++ src/main/java/com/runner/TestRunner.java
@@ -2,24 +2,48 @@ package com.runner;
 
 import com.context.SessionContext;
 import com.context.TestExecutionContext;
+import com.exceptions.InvalidTestDataException;
 import io.cucumber.core.cli.Main;
+
+import java.util.Set;
 
 public class TestRunner {
 
+    private static String platform = "";
+
     public TestRunner(){
-        run();
+        throw new InvalidTestDataException("Required args not provided to Runner");
+    }
+    public TestRunner(String[] args){
+        String configFilePath = args[0], tags = args[1];
+        platform = args[2];
+        Setup.load(configFilePath);
+        Setup.setConfigProperties();
+        run(tags);
     }
 
-    private void run() {
+    private void run(String tags) {
 
-        String[] array = { "--tags", "@indigo",
+        String[] array = { "--tags", tags,
                             "--plugin", "pretty",
                             "--plugin", "html:target/cucumber.html",
                             "--threads", "1",
                             "--plugin", "com.listeners.CucumberPlatformScenarioListener",
-                            "--glue", "com.steps",
+                            "--glue", "com.tests.steps",
                             "src/main/resources/features"};
         System.out.println("Inside Run -- ");
         byte status = Main.run(array);
+    }
+
+    static String getURL(){
+        return Setup.getURL();
+    }
+
+    public static String getPlatform(){
+        return platform;
+    }
+
+    public static String getConfig(String property){
+        return Setup.getConfig(property);
     }
 }
