296fb90528a6db3db6d48108642816326618c97b
Aman <amandaharwal@gmail.com>
2024-03-28 11:15:04 +0530

    Added TEST_CONTEXT

--- src/main/java/com/entities/TEST_CONTEXT.java
+++ src/main/java/com/entities/TEST_CONTEXT.java
@@ -2,5 +2,7 @@ package com.entities;
 
 public class TEST_CONTEXT {
 
-    public static final String BROWSER = "browser";
+    public static final String BROWSER = "BROWSER";
+    public static final String CURRENT_DRIVER = "currentDriver";
+    public static final String URL = "URL";
 }
