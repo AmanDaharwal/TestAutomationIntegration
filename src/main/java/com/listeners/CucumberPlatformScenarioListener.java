afde65ecb1faaafaf200cfee983c70954d081345
Aman.Daharwal <aman.daharwal@ril.com>
2024-04-04 14:28:37 +0530

    Implemented Extend Reporter

--- src/main/java/com/listeners/CucumberPlatformScenarioListener.java
+++ src/main/java/com/listeners/CucumberPlatformScenarioListener.java
@@ -1,19 +1,31 @@
 package com.listeners;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.context.TestExecutionContext;
 import com.runner.Drivers;
 import io.cucumber.plugin.ConcurrentEventListener;
+import com.aventstack.extentreports.Status;
 import io.cucumber.plugin.event.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class CucumberPlatformScenarioListener implements ConcurrentEventListener {
 
+    private final static Logger logger = LogManager.getLogger(CucumberPlatformScenarioListener.class);
+    private ExtentReports extent;
+    private ExtentSparkReporter spark;
+    private ExtentTest scenario;
+
     public CucumberPlatformScenarioListener(){
-        System.out.println("Inside CucumberPlatformScenarioListener constructor");
+        logger.info("CucumberPlatformScenarioListener");
+        extent = new ExtentReports();
     }
 
     @Override
     public void setEventPublisher(EventPublisher eventPublisher) {
-        System.out.println("setEventPublisher");
+        logger.info("set Event Publisher");
         eventPublisher.registerHandlerFor(TestRunStarted.class, this::testRunStartedHandler);
         eventPublisher.registerHandlerFor(TestCaseStarted.class, this:: testCaseStartedHandler);
         eventPublisher.registerHandlerFor(TestCaseFinished.class, this:: testCaseFinishedHandler);
@@ -21,21 +33,32 @@ public class CucumberPlatformScenarioListener implements ConcurrentEventListener
     }
 
     private void testRunStartedHandler(TestRunStarted event){
-        System.out.println("testRunStartedHandler");
+        logger.info("Test Run Started");
+        String pathOfOutputDirectory  = System.getProperty("OUTPUT_DIRECTORY");
+        spark = new ExtentSparkReporter(pathOfOutputDirectory + "/ExtendReport/ExtendReport.html");
+        extent.attachReporter(spark);
     }
 
     private void testCaseStartedHandler(TestCaseStarted event){
-        System.out.println("testCaseStartedHandler");
         String testCaseName = event.getTestCase().getName();
+        logger.info("Test Case Started - "+testCaseName);
         TestExecutionContext testExecutionContext = new TestExecutionContext(testCaseName);
+        scenario = extent.createTest(testCaseName);
     }
 
     private void testCaseFinishedHandler(TestCaseFinished event){
-        System.out.println("testCaseFinishedHandler");
         Drivers.closeBrowser();
+        String testCaseName = event.getTestCase().getName();
+        Status status = Status.PASS;
+        if (!event.getResult().getStatus().isOk()) {
+            status = Status.FAIL;
+        }
+        logger.info(status+ "Test Case Finished: " + testCaseName);
+        scenario.log(status, "Test Case Finished: " + testCaseName);
     }
 
     private void testRunFinishedHandler(TestRunFinished event){
-        System.out.println("testRunFinishedHandler");
+        extent.flush();
+        logger.info("Test Run Finished Handler");
     }
 }
