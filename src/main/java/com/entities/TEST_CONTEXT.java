802efdd90677aeaa2c75888d546a85a7100b7096
Aman.Daharwal <aman.daharwal@ril.com>
2024-04-09 11:35:51 +0530

    Added Support for ExtendTestLogger

--- src/main/java/com/entities/TEST_CONTEXT.java
+++ src/main/java/com/entities/TEST_CONTEXT.java
@@ -5,4 +5,5 @@ public class TEST_CONTEXT {
     public static final String BROWSER = "BROWSER";
     public static final String CURRENT_DRIVER = "currentDriver";
     public static final String URL = "URL";
+    public static final String EXTEND_TEST = "EXTEND_TEST";
 }
