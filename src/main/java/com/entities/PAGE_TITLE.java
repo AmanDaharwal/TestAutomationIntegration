381d0e68920d4e3a02d7d5bc336c26090df48885
Aman.Daharwal <aman.daharwal@ril.com>
2024-04-16 13:13:22 +0530

    added PAGE_TITLE

---
+++ src/main/java/com/entities/PAGE_TITLE.java
@@ -0,0 +1,11 @@
+package com.entities;
+
+public class PAGE_TITLE {
+    public final static String PRODUCTS = "PRODUCTS";
+    public final static String CART = "YOUR CART";
+    public final static String CHECKOUT_OVERVIEW = "CHECKOUT: OVERVIEW";
+    public final static String CHECKOUT_INFO = "CHECKOUT: INFORMATION";
+    public final static String CHECKOUT_COMPLETE = "CHECKOUT: COMPLETE!";
+    public final static String FINISH = "FINISH";
+    public static final String CHECKOUT_YOUR_INFO = "CHECKOUT: YOUR INFORMATION";
+}
