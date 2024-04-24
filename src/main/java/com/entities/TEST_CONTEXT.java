e92448b4ee0d097a79081952101e82730e5ba5ea
Aman.Daharwal <aman.daharwal@ril.com>
2024-04-24 18:56:48 +0530

    Added capabilities for browserstack web

--- src/main/java/com/entities/TEST_CONTEXT.java
+++ src/main/java/com/entities/TEST_CONTEXT.java
@@ -11,6 +11,7 @@ public class TEST_CONTEXT {
     public static final String CAPTURE_LOGS = "CAPTURE_LOGS";
     public static final String CAPABILITIES = "CAPS";
     public static final String RUN_IN_CI = "RUN_IN_CI";
+    public static final String RUN_IN_COULD_PLATFORM = "RUN_IN_COULD_PLATFORM";
     public static final String REMOTE_ADDRESS = "remoteAddress";
     public static final String DEVICE_NAME = "deviceName";
     public static final String APP_PATH = "appPath";
@@ -18,6 +19,7 @@ public class TEST_CONTEXT {
     public static final String BROWSER_STACK = "browserStack";
     public static final String APP = "app";
     public static final String PLATFORM_NAME = "platformName";
+    public static final String BROWSER_VERSION = "browserVersion";
     public static final String PROJECT_NAME = "projectName";
     public static final String BUILD_NAME = "buildName";
     public static final String APPIUM_VERSION = "appiumVersion";
