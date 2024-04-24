4972a6a2cd2f173268744006bc1a9f7975c0b192
Aman.Daharwal <aman.daharwal@ril.com>
2024-04-24 18:53:07 +0530

    Added support for Browser Stack web execution

--- src/main/java/com/runner/BrowserDriver.java
+++ src/main/java/com/runner/BrowserDriver.java
@@ -9,6 +9,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -16,7 +17,10 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -30,8 +34,12 @@ class BrowserDriver {
     static Driver createWebDriver(TestExecutionContext context) {
         WebDriver innerDriver;
         Browser browser = Browser.valueOf(TestRunner.getConfig(TEST_CONTEXT.BROWSER).toLowerCase());
-        if (Boolean.valueOf(TestRunner.getConfig(TEST_CONTEXT.RUN_IN_CI))) {
-            innerDriver = getRemoteWebDriverFor(browser);
+        if (Boolean.parseBoolean(TestRunner.getConfig(TEST_CONTEXT.RUN_IN_CI))) {
+            if (Boolean.parseBoolean(TestRunner.getConfig(TEST_CONTEXT.RUN_IN_COULD_PLATFORM))) {
+                innerDriver = getCloudRemoteWebDriverFor(browser);
+            } else {
+                innerDriver = getRemoteWebDriverFor(browser);
+            }
         } else {
             innerDriver = getWebDriverFor(browser);
         }
@@ -51,12 +59,15 @@ class BrowserDriver {
                 return new FirefoxDriver(getFirefoxOptions());
             case edge:
                 return new EdgeDriver(getEdgeOptions());
+            case safari:
+                return new SafariDriver(getSafariOptions());
+
         }
         throw new AutomationException("Incorrect browser name provided as " + browser);
     }
 
     private static WebDriver getRemoteWebDriverFor(Browser browser) {
-        String remoteUrl = "http://localhost:4444/wd/hub";
+        String remoteUrl = Drivers.getRemoteUrl();
         try {
             switch (browser) {
                 case chrome:
@@ -65,11 +76,41 @@ class BrowserDriver {
                     return new RemoteWebDriver(new URL(remoteUrl), getFirefoxOptions());
                 case edge:
                     return new RemoteWebDriver(new URL(remoteUrl), getEdgeOptions());
+                case safari:
+                    return new RemoteWebDriver(new URL(remoteUrl), getSafariOptions());
             }
         } catch (MalformedURLException exception) {
             throw new AutomationException("Unable to initialized RemoteWebDriver with url " + remoteUrl, exception);
         }
         throw new AutomationException("Incorrect browser name provided as " + browser);
+    }
+
+    private static WebDriver getCloudRemoteWebDriverFor(Browser browser) {
+        String remoteUrl = Drivers.getRemoteUrl();
+        try {
+            switch (browser) {
+                case chrome:
+                    return new RemoteWebDriver(new URL(remoteUrl), getChromeOptions().merge(getCapabilities()));
+                case firefox:
+                    return new RemoteWebDriver(new URL(remoteUrl), getFirefoxOptions().merge(getCapabilities()));
+                case edge:
+                    return new RemoteWebDriver(new URL(remoteUrl), getEdgeOptions().merge(getCapabilities()));
+                case safari:
+                    return new RemoteWebDriver(new URL(remoteUrl), getSafariOptions().merge(getCapabilities()));
+            }
+        } catch (MalformedURLException exception) {
+            throw new AutomationException("Unable to initialized RemoteWebDriver with url " + remoteUrl, exception);
+        }
+        throw new AutomationException("Incorrect browser name provided as " + browser);
+    }
+
+    private static MutableCapabilities getCapabilities() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        JSONObject browserStackOptions = getBrowserStackOptions();
+        capabilities.setBrowserName(browserStackOptions.getString(TEST_CONTEXT.BROWSER.toLowerCase()));
+        capabilities.setCapability(TEST_CONTEXT.BROWSER_VERSION, browserStackOptions.getString(TEST_CONTEXT.BROWSER_VERSION));
+        capabilities.setCapability(TEST_CONTEXT.PLATFORM_NAME, browserStackOptions.getString(TEST_CONTEXT.PLATFORM_NAME));
+        return capabilities;
     }
 
     private static ChromeOptions getChromeOptions() {
@@ -96,6 +137,10 @@ class BrowserDriver {
         return options;
     }
 
+    private static SafariOptions getSafariOptions() {
+        return new SafariOptions();
+    }
+
     static void quitDriver(TestExecutionContext context) {
         Driver driver = (Driver) context.getTestState(TEST_CONTEXT.DRIVER);
         try {
@@ -115,6 +160,18 @@ class BrowserDriver {
             return new JSONObject(new String(Files.readAllBytes(Paths.get(capabilitiesFiles))))
                     .getJSONObject(platform)
                     .getJSONArray("arguments");
+        } catch (IOException e) {
+            throw new InvalidTestDataException("Unable to read arguments from capabilities file at " + capabilitiesFiles);
+        }
+    }
+
+    private static JSONObject getBrowserStackOptions() {
+        String capabilitiesFiles = TestRunner.getConfig(TEST_CONTEXT.CAPABILITIES);
+        String platform = TestRunner.getPlatform().toString();
+        try {
+            return new JSONObject(new String(Files.readAllBytes(Paths.get(capabilitiesFiles))))
+                    .getJSONObject(platform)
+                    .getJSONObject("browserStack");
         } catch (IOException e) {
             throw new InvalidTestDataException("Unable to read arguments from capabilities file at " + capabilitiesFiles);
         }
